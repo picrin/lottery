d2654a7a89ecfc277f03d0622e637d0d8b27f4c5
Daniel Tsvetkov <danieltcv@gmail.com>
2016-06-27 11:25:52 +0100

    Loops

--- lottery.py
+++ lottery.py
@@ -1,20 +1,25 @@
 from random import randint
-x = randint(1,50)
-print(x)
+randomnumbers = []
+for x in range (0, 6):
+    y = randint(1,5)
+    randomnumbers.append(y)
+    print(y)
 
 
-try:
-	number = int(raw_input("Choose six numbers and enter them one by one from 1 to 49:"))
-	if not (1 <= number <= 49):
-		raise ValueError()
-except ValueError:
-	print "Invalid option, enter numbers from 1 to 49"
-	number = raw_input
-else:
-	print "Your number is", number
+for number in range (0,6):
+    z = number
+    try:
+        number = int(raw_input("Choose six numbers and enter them one by one from 1 to 49:"))
+        if not (1 <= number <= 49):
+            raise ValueError()
+    except ValueError:
+        print "Invalid option, enter numbers from 1 to 49"
+        number = raw_input
+    else:
+        print "Your number is", number
 
 
 if x == number :
-	print "You are lucky."
+    print "You are lucky."
 else:
-	print "You're not lucky today."
+    print "You're not lucky today."
