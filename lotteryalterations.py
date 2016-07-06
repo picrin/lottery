88a8e15aad2ec228bab86875dfca1412d253c01c
Gosia <you@example.com>
2016-07-06 19:30:00 +0100

    additional comparison of numbers

---
+++ lotteryalterations.py
@@ -0,0 +1,9 @@
+for y in randomnumbers:
+    if y == randomnumbers1[0]:
+    if y == randomnumbers1[1]:
+    if y == randomnumbers1[2]:
+    if y == randomnumbers1[3]:
+    if y == randomnumbers1[4]:
+    if y == randomnumbers1[5]:
+    if y == randomnumbers1[6]:
+    print "whatever"
