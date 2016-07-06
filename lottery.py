b81993a170db6e8203a4ef1ebb3e0962905c351e
Daniel Tsvetkov <danieltcv@gmail.com>
2016-07-06 19:42:48 +0100

    gosia fixed a bug

--- lottery.py
+++ lottery.py
@@ -1,15 +1,12 @@
 from random import randint
 randomnumbers = []
 for y in range (0, 6):
-    y = randint(1,5)
+    y = randint(1,50)
     randomnumbers.append(y)
     print(y)
 
 randomnumbers1 = []
 for z in range (0,6):
-    z = number(1,5)
-    randomnumbers1.append(z)
-    
     try:
         number = int(raw_input("Choose six numbers and enter them one by one from 1 to 49:"))
         if not (1 <= number <= 49):
@@ -18,14 +15,14 @@ for z in range (0,6):
         print "Invalid option, enter numbers from 1 to 49"
         number = raw_input
     else:
+        randomnumbers1.append(number)
         print "Your number is", number
 
 
 for i in randomnumbers: 
     for j in randomnumbers1:
         if i == j:
-            print "whatever"
-
+            print "number %i matches %i" % (i, j)
 
 
 
